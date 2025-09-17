aac4a2febfd2ec91e510837559da9679d36e29aa
Ranjit <ranjit.choudhary123@outlook.com>
2025-09-17 17:10:04 +0530

    security updates

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
-// src/lib/supabase.ts
-import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY!
 
-const supabaseUrl = 'https://nggvqjtvuwtcpvjvvwvz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5nZ3ZxanR2dXd0Y3B2anZ2d3Z6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyODk5OTYsImV4cCI6MjA3MDg2NTk5Nn0.fRtkLfGlHdhm59VEjNndljQ8ZL9-jlmgdfIsd08sdgg'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
